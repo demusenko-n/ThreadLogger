720da2b0048cafdb0d53b87fb8fb59f85f4853a6
Демусенко Микита <mykyta.demusenko@nure.ua>
2021-12-03 17:11:05 +0200

    Add Active and Message classes

---
+++ Active.cpp
@@ -0,0 +1,41 @@
+﻿#include "Active.h"
+active_object::Active::Message::~Message() = default;
+
+void active_object::Active::Message::Execute()
+{}
+
+active_object::Active::Active(): thd_(std::make_unique<thread>(&Run))
+{}
+
+active_object::Active::~Active()
+{
+	try
+	{
+		Interrupt();
+		thd_->join();
+	}
+	catch (...)
+	{}
+}
+
+void active_object::Active::Send(unique_ptr<Message> m)
+{
+	if (m != nullptr)
+	{
+		mq_.push(m);
+	}
+}
+
+void active_object::Active::Run()
+{
+	unique_ptr<Message> msg;
+	while ((msg = mq_.dequeue()) != nullptr)
+	{
+		msg->Execute();
+	}
+}
+
+void active_object::Active::Interrupt()
+{
+	mq_.push(nullptr);
+}
---
+++ Active.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <memory>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <queue>
+
+#include "QueueThreadSafe.h"
+
+namespace active_object
+{
+	using std::unique_ptr;
+	using std::queue;
+	using std::thread;
+
+
+	class Active
+	{
+	public:
+		class Message
+		{
+		public:
+			virtual ~Message();
+			virtual void Execute();
+		};
+
+		Active();
+
+		~Active();
+
+		void Send(unique_ptr<Message> m);
+
+		//Active(Active&& other) = delete;
+		//Active(const Active& other) = delete;
+		Active& operator= (const Active& other) = delete;
+		Active& operator= (Active&& other) = delete;
+	private:
+		QueueThreadSafe<unique_ptr<Message>> mq_;
+		unique_ptr<thread> thd_;
+
+		void Run();
+
+		void Interrupt();
+	};
+}
