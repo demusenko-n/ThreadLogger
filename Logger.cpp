e73ebcb36f88f68e1396f8e2d01a81e6f451cc58
mdemusenko <mykyta.demusenko@nure.ua>
2022-07-26 14:28:20 +0300

    namespace declaration .cpp file

--- Logger.cpp
+++ Logger.cpp
@@ -2,40 +2,43 @@
 #include <chrono>
 #include <iostream>
 
-logger::LoggerIndentManager::LoggerIndentManager()
-{
-	++indent_count_;
+namespace logger {
+	LoggerIndentManager::LoggerIndentManager()
+	{
+		++indent_count_;
+	}
+
+	LoggerIndentManager::~LoggerIndentManager()
+	{
+		--indent_count_;
+	}
+
+	void LoggerIndentManager::Log(const std::string& file, const std::string& function, int line, const std::string& message)
+	{
+		logger::LoggerInternal::GetLogger().Log(file, function, line, message, indent_count_);
+	}
+
+	LoggerInternal::LogMessage::LogMessage(std::string s) : msg_(std::move(s)), is_final_msg_(false)
+	{}
+
+	bool LoggerInternal::LogMessage::IsStopMessage() {
+		return is_final_msg_;
+	}
+
+	const LoggerInternal::LogMessage& LoggerInternal::LogMessage::GetStopMessage()
+	{
+		static LogMessage msg_final;
+		return msg_final;
+	}
+
+	void LoggerInternal::LogMessage::Execute()
+	{
+		using namespace std::chrono_literals;
+
+		std::cout << msg_ << std::endl;
+		std::this_thread::sleep_for(100ms);
+	}
+
+	int thread_local LoggerIndentManager::indent_count_ = -1;
+
 }
-
-logger::LoggerIndentManager::~LoggerIndentManager()
-{
-	--indent_count_;
-}
-
-void logger::LoggerIndentManager::Log(const std::string& file, const std::string& function, int line, const std::string& message)
-{
-	logger::LoggerInternal::GetLogger().Log(file, function, line, message, indent_count_);
-}
-
-logger::LoggerInternal::LogMessage::LogMessage(std::string s): msg_(std::move(s)), is_final_msg_(false)
-{}
-
-bool logger::LoggerInternal::LogMessage::IsStopMessage() {
-	return is_final_msg_;
-}
-
-const logger::LoggerInternal::LogMessage& logger::LoggerInternal::LogMessage::GetStopMessage()
-{
-	static LogMessage msg_final;
-	return msg_final;
-}
-
-void logger::LoggerInternal::LogMessage::Execute()
-{
-	using namespace std::chrono_literals;
-	
-	std::cout << msg_ << std::endl;
-	std::this_thread::sleep_for(100ms);
-}
-
-int thread_local logger::LoggerIndentManager::indent_count_ = -1;
